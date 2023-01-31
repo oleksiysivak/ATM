c888267e603baed7e58c8e638d8731e7de57617f
olesksiysivak <123511184+olesksiysivak@users.noreply.github.com>
2023-01-31 22:28:47 +0000

    new file:   Excer 	modified:   fizzbuzz 	modified:   fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -10,7 +10,7 @@ int main() {
         if (i % 3 == 0) {
         printf ("Fizz\n");}
 
-       else if (i % 5 == 0) {
+       if (i % 5 == 0) {
         printf ("Buzz\n");} 
 
         else if (i % 15 == 0) {
