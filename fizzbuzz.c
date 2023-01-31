d48e756a3792f04bd7fd1b7a71904e7016589c08
olesksiysivak <123511184+olesksiysivak@users.noreply.github.com>
2023-01-31 22:35:54 +0000

    modified:   fizzbuzz 	modified:   fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -7,17 +7,18 @@ int main() {
     printf ("Please enter your number: ");
     scanf ("%d", &num);
     for (int i = 1; i <= num; i++){
-        if (i % 3 == 0) {
-        printf ("Fizz\n");}
-
-       if (i % 5 == 0) {
-        printf ("Buzz\n");} 
-
-        else if (i % 15 == 0) {
+    
+    if (i % 15 == 0) {
             printf ("FIZZBUZZ\n");}
 
-        else 
-        printf ("%d\n",i);
+    else if (i % 5 == 0) {
+            printf ("BUZZ\n");} 
+    
+    else if (i % 3 == 0) {
+            printf ("FIZZ\n");}
+
+    else 
+            printf ("%d\n",i);
     }
 return 0;
 }
