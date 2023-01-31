139317355320e96662b61be2e5b83cbc8fe7c198
olesksiysivak <123511184+olesksiysivak@users.noreply.github.com>
2023-01-31 21:49:43 +0000

    Excer

---
+++ fizzbuzz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+
+    int num;
+
+    printf ("PLease enter your number: ");
+    scanf (%d; &i)
+    for (int i = 1; i <= 0; i++){
+        if (i % 3 == 0) {
+        printf ("fizz");}
+
+        else if (i % 5==0) {
+        printf ("buzz");}
+
+        else 
+        printf ("%d",i);
+    }
